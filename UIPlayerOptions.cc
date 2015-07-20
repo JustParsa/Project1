775e3407332c5215f63551d7d492f06073ea3339
Parsa Alipour <parsaalipour@hotmail.com>
2015-07-19 22:26:48 -0400

    completely done

--- UIPlayerOptions.cc
+++ UIPlayerOptions.cc
@@ -18,22 +18,26 @@ UIPlayerOptions::UIPlayerOptions(View& view, Game& model) : view_(view), model_(
 		playerFrames[i].add(*playerOptions[i]);
 		Gdk::Color gray("gray");
 		playerFrames[i].modify_bg(Gtk::STATE_NORMAL, gray);
+		// playerFrames[i].override_color(gray, Gtk::STATE_NORMAL);
 	}
-
 }
 
 // update each UI element for each player
 void UIPlayerOptions::update() {
+	std::cerr<<"Updating options"<<std::endl;
 	int currPlayerIndex = model_.getCurrentPlayer();
 	Gdk::Color green("green");
 	Gdk::Color gray("gray");
 	for (int i = 0; i < 4; ++i) {
 		playerOptions[i]->update();
+		std::cerr<<"We're at player "<<i<<std::endl;
 		if (i == currPlayerIndex) {
+			std::cerr<<"Set something to green"<<std::endl;
 			playerFrames[currPlayerIndex].modify_bg(Gtk::STATE_NORMAL, green);
 		}
 		else {
-			playerFrames[currPlayerIndex].modify_bg(Gtk::STATE_NORMAL, gray);
+			std::cerr<<"Set something to gray"<<std::endl;
+			playerFrames[i].modify_bg(Gtk::STATE_NORMAL, gray);
 		}
 	}
 }
